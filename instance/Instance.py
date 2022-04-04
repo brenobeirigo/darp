269be5ea7e7d0bd1bc03575688104129c2472dbf
brenobeirigo <brenobeirigo@gmail.com>
2022-04-04 16:51:27 +0200

    get instance data

--- instance/Instance.py
+++ instance/Instance.py
@@ -1,6 +1,6 @@
 from model.Request import Request
 from model.Vehicle import Vehicle
-from model.Node import Node
+from model.Node import Node, OriginNode, PickupNode, DestinationNode, DropoffNode
 
 class Instance:
     def __init__(
@@ -16,9 +16,54 @@ class Instance:
 
         self.nodes = nodes
         self.node_id_dict = {n.pos:n for n in self.nodes}
-
+        
+        self.node_id_pos_dict = {n.id:n.pos for n in self.nodes}
+        
+        self.pickup_nodes = []
+        self.dropoff_nodes = []
+        self.destination_nodes = []
+        self.origin_nodes = []
+        
+        for n in self.nodes:
+            if type(n) == PickupNode:
+                self.pickup_nodes.append(n)
+            elif type(n) == DropoffNode:
+                self.dropoff_nodes.append(n)
+            elif type(n) == DestinationNode:
+                self.destination_nodes.append(n)
+            elif type(n) == OriginNode:
+                self.origin_nodes.append(n)
+        
+        dist = {}
+        for o in self.node_id_pos_dict:
+            dist[o] = {}
+            o_pos = self.node_id_pos_dict[o]
+            for d in self.node_id_pos_dict:
+                d_pos = self.node_id_pos_dict[d]
+                dist[o][d] = dist_matrix[o_pos][d_pos]
+                    
+        self.dist_matrix_id = dist
         self.dist_matrix = dist_matrix
-    
+        
+        
+    def get_data(self):
+        TOTAL_HORIZON = 1440
+        data = dict(
+            origin_depot=self.vehicles[0].pos,
+            K=[v.id for v in self.vehicles],
+            Q={v.id:v.capacity for v in self.vehicles},
+            P=[n.pos for n in self.pickup_nodes],
+            D=[n.pos for n in self.dropoff_nodes],
+            L={r.pickup_node.pos:r.max_ride_time for r in self.requests},
+            el={n.pos:n.el for n in self.nodes},
+            d={n.pos:n.service_delay for n in self.nodes},
+            q={n.pos: n.load for n in self.nodes},
+            dist_matrix=self.dist_matrix,
+            total_horizon=TOTAL_HORIZON
+        )
+        
+        return data
+
     def __del__(self):
         '''Resets the id count for requests, vehicles, and nodes'''
         Request.cleanup()
