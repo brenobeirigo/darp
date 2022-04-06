dd6c8c9152307d8d45988e64f04e0b5750e36cf8
Breno <brenobeirigo@gmail.com>
2022-04-06 08:43:31 +0200

    project config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='darp',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Dial-a-ride problem solvers',
+    author='Breno A. Beirigo',
+    license='MIT',
+)
