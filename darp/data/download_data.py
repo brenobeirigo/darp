c09097d26185e605467ad55a70d6f80a8e1cf170
Breno <brenobeirigo@gmail.com>
2022-04-06 08:44:33 +0200

    fixed download data

--- darp/data/download_data.py
+++ darp/data/download_data.py
@@ -2,16 +2,24 @@ import csv
 import re
 import os
 
-with open('benchmark_instances.csv', newline='') as csvfile:
+
+FOLDER_DATA_RAW_INSTANCES = os.path.join(
+    os.getcwd(), "data","raw", "darp_instances")
+
+FILEPATH_DATA_SOURCE = os.path.join(
+    os.getcwd(), "darp", "data", "benchmark_instances.csv")
+
+with open(FILEPATH_DATA_SOURCE, newline='') as csvfile:
     instances = csv.DictReader(csvfile)
     for row in instances:
         author = row["First reference"].split()[0].lower()
         code = row["Code"].lower()
         year = re.findall("[0-9]+", row["First reference"])[0]
         link = row["Link"]
-        
-        
-        folder = f"data/{code}_{author}_{year}"
+
+        folder = os.path.join(
+            FOLDER_DATA_RAW_INSTANCES,
+            f"{code}_{author}_{year}")
         # -r: recursively download content
         # nH: no host
         # cut-dirs: cut the folder path until last folder
@@ -19,7 +27,5 @@ with open('benchmark_instances.csv', newline='') as csvfile:
         # -P: create target folder
         command = f"wget -r -np -nH --cut-dirs={cut_dirs} {link} -P {folder}"
         print(f"Downloading {link} to {folder} ({command})...")
-        
+
         os.system(command)
-        
-        
